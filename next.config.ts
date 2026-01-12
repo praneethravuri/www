66f5fcc4365857dcd882651c6f9c549cb9cfcbbb
Praneeth Ravuri <prav2500@gmail.com>
2026-01-12 10:18:16 -0800

    performance improvements

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  compress: true,
+  poweredByHeader: false,
   experimental: {
     optimizePackageImports: ["lucide-react", "react-icons", "motion"],
   },

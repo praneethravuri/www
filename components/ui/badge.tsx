b69b81a9971b74374bd582fe92895ce1c086d17d
Praneeth Ravuri <prav2500@gmail.com>
2026-01-13 23:16:35 -0800

    updated

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -10,13 +10,13 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default:
-          "border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90",
+          "border-transparent bg-primary/20 text-primary hover:bg-primary/20 hover:text-primary",
         secondary:
-          "border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90",
+          "border-transparent bg-secondary/50 text-muted-foreground/80 hover:bg-secondary/50 font-mono font-normal",
         destructive:
-          "border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+          "border-transparent bg-destructive text-white hover:bg-destructive hover:text-white",
         outline:
-          "text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
+          "text-foreground hover:bg-transparent hover:text-foreground",
       },
     },
     defaultVariants: {
